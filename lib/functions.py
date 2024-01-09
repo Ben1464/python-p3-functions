e5fcff2428c266037eb9f69b437099d43a06af7b
Benson Mwangi <kimaniben@gmail.com>
2024-01-09 16:20:26 -0500

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
-#!/usr/bin/env python3
-
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+def add(a, b):
+    result = a + b
+    print(f"{a} + {b} = {result}")
+    return result
 
-def halve(number):
-    pass
+def halve(num):
+    result = num / 2
+    print(f"Half of {num} is {result}")
+    return result
