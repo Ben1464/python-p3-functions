cea7f5846629c9355ebf65d4d00cee2b44cebee1
Benson Mwangi <kimaniben@gmail.com>
2024-01-11 11:24:49 +0000

    Practice

--- lib/functions.py
+++ lib/functions.py
@@ -16,3 +16,28 @@ def halve(num):
     result = num / 2
     print(f"Half of {num} is {result}")
     return result
+def hallo():
+    print("Morning greatings ")
+def multiply(d,e):
+    result = d*e
+    print(f"{d} * {e} = {result}")
+    return result
+
+my_name = "Ben"
+print(f"hello{my_name}")
+
+school = "Moringa"
+print(f"Which school are you studying in {school}")
+
+def moringa():
+    print("Hello moringa ")
+
+
+price_1 = 12
+price_2 = 14
+print(f"The pencil cost{price_1}")
+print(f"The pen cost{price_2}")
+
+
+
+
